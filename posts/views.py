fc4106a3fdcc3b7966d9e816eee583b878646b6b
Princr <N01416165H@students.nust.ac.zw>
2018-01-17 08:03:23 +0200

    First Push

--- posts/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
-import datetime
-
-def index(request):
-    return render_to_response(request,'posts/index.html')
